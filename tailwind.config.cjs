6c29fa417a7f929317b96d5df636738a539c891a
Cesc Fàbregas <cesc@cescfabregasestudi.com>
2023-07-27 20:45:55 +0200

    Fase 2 Tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
+		fontFamily: {
+			body: ["Söhne"],
+			display: ["SöhneBreit"],
+		},
+		colors: {
+			transparent: "transparent",
+			current: "currentColor",
+			primary: "#ffe233",
+			secondary: "#f7f5e9",
+			black: "#0f0d03",
+		},
 		extend: {},
 	},
 	plugins: [],
-}
+};
